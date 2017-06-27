2c1e70ab6b0b35891e70407f638b67d72410db91
anonymoussc <50c5ac69@opayq.com>
2017-06-27 19:03:09 +0700

    Deleted routes.php

--- src/Http/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'signature', 'namespace' => 'App\\Components\Signature\Http\Controllers'], function () {
-    Route::get('/', 'SignatureController@index');
-});
