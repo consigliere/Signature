76ce743f46382f0d6c703075c29f70780b9f687c
anonymoussc <50c5ac69@opayq.com>
2017-10-22 03:19:43 +0700

    Modified web Routes

--- Routes/web.php
+++ Routes/web.php
@@ -5,5 +5,5 @@
  */
 
 Route::group(['middleware' => 'web', 'prefix' => 'signature', 'namespace' => 'App\\Components\Signature\Http\Controllers'], function () {
-    Route::get('/', 'SignatureController@index');
+    // Route::get('/', 'SignatureController@index');
 });
