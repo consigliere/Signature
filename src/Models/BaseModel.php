6f0ab481a24d79439b25fea8bd921d44fc89050d
anonymoussc <50c5ac69@opayq.com>
2018-04-25 01:07:09 +0700

    Modified BaseModel

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -9,13 +9,10 @@ namespace App\Components\Signature\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Components\Signature\Traits\QueryBasic;
 use App\Components\Signature\Traits\Pagination;
-use App\Components\Signal\Traits\Signal;
 
 class BaseModel extends Model
 {
-    use QueryBasic;
-    use Pagination;
-    use Signal;
+    use QueryBasic, Pagination;
 
     protected $fillable = [];
 }
