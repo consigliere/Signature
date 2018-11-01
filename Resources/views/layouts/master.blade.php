151729df0b01e70160c8e873e0bbec870ba48cf5
anonymoussc <50c5ac69@opayq.com>
2018-11-01 14:03:48 +0700

    Update Resources

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -4,9 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Component Signature</title>
+        <title>Module Signature</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/signature.css') }}"> --}}
+
     </head>
     <body>
         @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/signature.js') }}"></script> --}}
     </body>
 </html>
