49e00ebf089e7e7e089bd1f9e187aca22a1d990b
anonymoussc <50c5ac69@opayq.com>
2019-07-10 06:14:51 +0700

    refactor(Helpers): modified ./Helpers/Helpers

--- Helpers/Helpers.php
+++ Helpers/Helpers.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Copyright(c) 2019. All rights reserved.
- * Last modified 5/21/19 2:54 PM
+ * Helper.php
+ * Created by @anonymoussc on 05/21/2019 10:38 AM.
  */
 
 /**
- * Helper.php
- * Created by @anonymoussc on 05/21/2019 10:38 AM.
+ * Copyright(c) 2019. All rights reserved.
+ * Last modified 7/10/19 6:14 AM
  */
 
 if (!function_exists('randomUuid')) {
@@ -17,5 +17,18 @@ if (!function_exists('randomUuid')) {
     function randomUuid()
     {
         return (string)\Webpatser\Uuid\Uuid::generate(4);
+    }
+}
+
+if (!function_exists('httpStatusCode')) {
+
+    /**
+     * @param $e
+     *
+     * @return int
+     */
+    function httpStatusCode($e)
+    {
+        return method_exists($e, 'getStatusCode') ? $e->getStatusCode() : 500;
     }
 }
